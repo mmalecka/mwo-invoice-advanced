5cc519e4cda01edbfbc4eb62c2e0fb8d7a63cdac
mmalecka <m.malecka@poczta.fm>
2017-02-21 22:04:00 +0100

    Plik Invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,27 +1,56 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
+	private Collection<Product> products; 
+	
+	public Invoice(){
+		products = new ArrayList<Product>();
+	}
+	
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.add(product);
 	}
 
-	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+	public void addProduct(Product product, Integer quantity) throws IllegalArgumentException {
+		if (quantity <= 0){
+			throw new IllegalArgumentException ("blad");
+		}
+		
+		for(int i =0;i<quantity;i++){
+			products.add(product);
+		}
 	}
 
 	public BigDecimal getSubtotal() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for(Product product : products){
+			sum = sum.add(product.getPrice());
+		}
+		
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for(Product product : products){
+			sum = sum.add(product.getPriceWithTax().subtract(product.getPrice()));
+		}
+		
+		return sum;
 	}
 
 	public BigDecimal getTotal() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for(Product product : products){
+			sum = sum.add(product.getPriceWithTax());
+		}
+		
+		return sum;
 	}
 }
